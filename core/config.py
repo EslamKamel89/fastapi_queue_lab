b869625c9c5f44fd0da47ed261054501742a231d
EslamKamel89 <elsamkamelforex@gmail.com>
2026-02-06 16:36:45 +0200

    feat(config): add DATABASE_URL to application settings

--- core/config.py
+++ core/config.py
@@ -5,6 +5,7 @@ from pydantic import BaseModel
 
 class Settings(BaseModel):
     APP_NAME: str | None = os.getenv("APP_NAME")
+    DATABASE_URL: str | None = os.getenv("DATABASE_URL")
 
 
 settings = Settings()
